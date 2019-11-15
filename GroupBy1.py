8f061c8d812acc551418e2372f5cdd3d5640ce28
Dani Monteiro DBA <danimonteirodba@gmail.com>
2019-11-15 17:02:20 -0300

    Add files via upload

---
+++ GroupBy1.py
@@ -0,0 +1,26 @@
+﻿import pymongo
+import pprint
+
+#Criar um client
+from pymongo import MongoClient
+
+#conectar no MongoDB
+client = MongoClient(STRING DE CONEXÃO COM O MONGODB)
+
+#Criar a variável db
+db = client.sample_mflix.movies
+
+cursor_GroupBy_Contagem = db.aggregate([
+    {
+        '$group': {
+            '_id': '$runtime', 
+            'contagem': {
+                '$sum': 1
+            }
+        }
+    }
+])
+   
+for doc in cursor_GroupBy_Contagem:
+    pprint.pprint(doc)
+ 
